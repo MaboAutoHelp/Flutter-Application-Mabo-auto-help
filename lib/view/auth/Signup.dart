46e13a527af42fca390746fa3c43996ca0ae96de
mohamed-aziz <medazizzammal@gmail.com>
2024-07-31 20:32:38 +0100

    chore: Add http package dependency and implement login authentication functionality

---
+++ lib/view/auth/Signup.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:mabo_auto_help/view/auth/Login.dart';
+
+class Signup extends StatefulWidget {
+  Signup({super.key});
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  TextEditingController email = TextEditingController();
+  TextEditingController pwd = TextEditingController();
+  TextEditingController name = TextEditingController();
+
+  GlobalKey<FormState> fromstate = GlobalKey<FormState>();
+
+  Signup() {
+    var formData = fromstate.currentState;
+
+    if (formData!.validate()) {
+      print('Form is valid');
+      // print(name.text);
+      // print(email.text);
+      // print(pwd.text);
+    } else {
+      print('Form is invalid');
+    }
+  }
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: fromstate,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Mabo auto help',
+              style: TextStyle(
+                  fontSize: 30, color: Color.fromARGB(255, 244, 59, 3)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 30, color: Colors.lightBlue),
+                  ),
+                ],
+              ),
+            ),
+
+            ///------------------Name---------------------
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: name,
+                validator: (val) {
+                  if (val!.isEmpty) {
+                    return 'name is required';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "Name",
+                  prefixIcon: Icon(Icons.person),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+
+            ///------------------Email---------------------
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: email,
+                validator: (val) {
+                  if (val!.isEmpty) {
+                    return 'Email is required';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: Icon(Icons.email),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+
+            ///-------------------Pwd--------------------
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: pwd,
+                validator: (val) {
+                  if (val!.isEmpty) {
+                    return 'Password is required';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "password",
+                  prefixIcon: Icon(Icons.password),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+
+            ///--------------------------------------
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Signup();
+                 
+                },
+                child: Text('Sign Up'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Login()));
+                    },
+                    child: Text(
+                      'Login->',
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
