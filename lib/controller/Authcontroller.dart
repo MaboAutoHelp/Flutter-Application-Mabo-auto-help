bdf081242ce61a9f62e93720d2f24fb7688d3a78
mohamed-aziz <medazizzammal@gmail.com>
2024-08-08 15:55:51 +0100

    feat: Implement signup functionality and API integration

--- lib/controller/Authcontroller.dart
+++ lib/controller/Authcontroller.dart
@@ -13,4 +13,19 @@ class Authcontroller {
       return data;
     }
   }
+  Future SignupAuth(name ,email, pwd,tel) async {
+    var url = "http://192.168.1.21:8000/users/register";
+    var response = await http.post(Uri.parse(url),
+        body: jsonEncode(<String, String>{
+          'name': name,
+          'email': email,
+        
+          'pwd': pwd,
+          'tel':tel
+           }), headers: {"Content-Type": "application/json"});
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return data;
+    }
+}
 }
