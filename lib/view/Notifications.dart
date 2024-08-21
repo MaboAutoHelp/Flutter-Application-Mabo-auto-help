18eaea55b9cd1d4a0aca7b7c48e9bf8df9b4052e
mohamed-aziz <medazizzammal@gmail.com>
2024-08-21 22:37:29 +0100

    chore: Update Notifications.dart to include QR code functionality

--- lib/view/Notifications.dart
+++ lib/view/Notifications.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
 import 'package:intl/intl.dart'; // استيراد مكتبة intl
 import 'package:mabo_auto_help/controller/NotificationsController.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
 class Notifications extends StatefulWidget {
   final String userID;
   const Notifications({super.key, required this.userID});
@@ -17,20 +18,6 @@ class _NotificationsState extends State<Notifications> {
   void initState() {
     super.initState();
     notifications = Notificationscontroller.getNotifications(widget.userID);
-    
-    // تأكيد بدء تنفيذ الـ Future
-    /*print('Fetching notifications...');
-    
-    notifications.then((data) {
-      // طباعة البيانات إذا نجح الجلب
-      print('Notifications fetched: $data');
-    }).catchError((error) {
-      // طباعة الخطأ إذا حدث
-      print('Error fetching notifications: $error');
-    }).whenComplete(() {
-      // تأكيد اكتمال العملية
-      print('Fetch operation completed');
-    });*/
   }
 
   String formatDate(String dateStr) {
@@ -47,13 +34,10 @@ class _NotificationsState extends State<Notifications> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            print('Error: ${snapshot.error}');
             return Center(child: Text('Failed to load notifications'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            print('No notifications found');
             return Center(child: Text('No notifications found'));
           } else {
-            print('Notifications Data: ${snapshot.data}');
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
@@ -61,15 +45,46 @@ class _NotificationsState extends State<Notifications> {
                 return Card(
                   margin: EdgeInsets.all(8.0),
                   child: ListTile(
-                    title: Text(notification['serviceName'] ?? 'No service name'),
+                    title:
+                        Text(notification['serviceName'] ?? 'No service name'),
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text('Date: ${formatDate(notification['date'])}'),
                         Text('Time: ${notification['time'] ?? 'No time'}'),
-                        Text('Car Type: ${notification['carType'] ?? 'No car type'}'),
-                        Text('Ita Type: ${notification['ita'] ?? 'No ita type'}'),
+                        Text(
+                            'Car Type: ${notification['carType'] ?? 'No car type'}'),
+                        Text(
+                            'Ita Type: ${notification['ita'] ?? 'No ita type'}'),
                       ],
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.qr_code),
+                      onPressed: () {
+                        if (notification['_id'] != null) {
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text('QR Code'),
+                              content: SizedBox(
+                                width: 200.0, // عرض محدد
+                                height: 200.0, // ارتفاع محدد
+                                child: QrImageView(
+                                  data: notification['_id'], // البيانات المستلمة من الخادم
+                                  version: QrVersions.auto,
+                                  size: 200.0,
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () => Navigator.of(context).pop(),
+                                  child: Text('Close'),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      },
                     ),
                   ),
                 );
@@ -81,6 +96,7 @@ class _NotificationsState extends State<Notifications> {
     );
   }
 }
+
 
 /*class Notifications extends StatefulWidget {
   final String userID;
